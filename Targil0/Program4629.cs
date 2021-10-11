9607f8128da00b6e13acb99e3f9e5496e0697bdb
rivkif770 <frieman@g.jct.ac.il>
2021-10-11 16:49:51 +0300

    initial code + Welcome functions

---
+++ Targil0/Program4629.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome4629();
+            Welcome9444();
+            Console.ReadKey();
+        }
+        static partial void Welcome9444();
+        private static void Welcome4629()
+        {
+            Console.WriteLine("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", userName);
+        }
+    }
+}
