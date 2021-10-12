502cd36643e7d27d336a9ce8f6bdc9b099c0e13c
ehilel <efrat10770@gmail.com>
2021-10-12 21:35:18 +0300

    I am here - Efrat

---
+++ ConsoleUI/Program19444.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    partial class Program_1_
+    {
+        static partial void Wellcome9444()
+        {
+            Console.WriteLine("I am also here!");
+        }
+    }
+}
