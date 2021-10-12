d4baa069ad88fee3a8be248c906ef5eee8fb6a4a
rivkif770 <frieman@g.jct.ac.il>
2021-10-12 20:34:39 +0300

    initial code+welcome functions

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stam();
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        private static void Stam()
+        {
+            IDAL.DO.Client client = new IDAL.DO.Client
+            {
+                Name = "kuku",
+                Latitude = -36.123456,
+                Longitude = 29.654321,
+                Telephone = "052534111",
+                ID = 123
+            };
+            Console.WriteLine(client);
+        }
+    }
+}
---
+++ DAL/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace IDAL
+{
+    namespace DO
+    {
+        public struct Client
+        {
+            public int ID { get;  set; }
+            public String Name { get; set; }
+            public String Telephone { get; set; }
+            public double Latitude { get; set; }
+            public double Longitude { get; set; }
+
+            public override string ToString()
+            {
+                String result = "";
+                result += $"ID is {ID}, \n";
+                result += $"Name is {Name}, \n";
+                result += $"Telephone is {Telephone.Substring(0,3)+'-'+Telephone.Substring(3)}, \n";
+                result += $"Latitude is {Latitude}, \n";
+                result += $"Longitude is {Longitude}, \n";
+                return result;
+            }
+        }
+    }
+}
