c7e9d3ce2d3103c6ccca6fc0ddf120ca46320eb8
rivkif770 <frieman@g.jct.ac.il>
2021-10-14 15:59:09 +0300

    new structs

--- DAL/BaseSation.cs
+++ DAL/BaseSation.cs
@@ -3,20 +3,20 @@ namespace IDAL
 {
     namespace DO
     {
-        public struct Client
+        public struct BaseStation
         {
-            public int ID { get;  set; }
-            public String Name { get; set; }
-            public String Telephone { get; set; }
+            public int UniqueID { get; set; }
+            public String StationName { get; set; }
+            public int SeveralPositionsArgument { get; set; }
             public double Latitude { get; set; }
             public double Longitude { get; set; }
 
             public override string ToString()
             {
                 String result = "";
-                result += $"ID is: {ID}, \n";
-                result += $"Name is: {Name}, \n";
-                result += $"Telephone is: {Telephone.Substring(0,3)+'-'+Telephone.Substring(3)}, \n";
+                result += $"Unique ID number is: {UniqueID}, \n";
+                result += $"The station name is: {StationName}, \n";
+                result += $"Several positions of argument is: {SeveralPositionsArgument}, \n";
                 result += $"Latitude is: {Latitude}, \n";
                 result += $"Longitude is: {Longitude}, \n";
                 return result;
---
+++ DAL/SkimmerLoading.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace IDAL
+{
+    namespace DO
+    {
+        public struct SkimmerLoading
+        {
+            public int StationID { get; set; }
+            public int SkimmerID { get; set; }
+
+            public override string ToString()
+            {
+                String result = "";
+                result += $"Base station ID is: {StationID}, \n";
+                result += $"Skimmer ID is: {SkimmerID}, \n";
+                return result;
+            }
+        }
+    }
+}
