5fcb45900da1c1a0156bdc1990e54979828b4a49
rivkif770 <frieman@g.jct.ac.il>
2021-10-17 20:50:25 +0300

    change 2

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -2,15 +2,24 @@
 
 namespace ConsoleUI
 {
+    enum Options {add, update, display, ViewTheLists, exit }
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Choose one of the following:\n add:Insert options\n update:Update options\n display:Display options\n ViewTheLists:Options List view\n exit:Output ");
+            string ans;
+            ans = Console.ReadLine();
+            switch (ans){
+                case add: AddStation(BaseStation)
+            }
+            AddStation(BaseStation);
             Stam();
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
         }
-
+        public void AddStation(BaseStation) 
+        { }
         private static void Stam()
         {
             IDAL.DO.Client client = new IDAL.DO.Client
--- ConsoleUI/Program19444.cs
+++ ConsoleUI/Program19444.cs
@@ -9,9 +9,9 @@ namespace ConsoleUI
 {
     partial class Program_1_
     {
-        static partial void Wellcome9444()
-        {
-            Console.WriteLine("I am also here!");
-        }
+        //static partial void Wellcome9444()
+        //{
+        //    Console.WriteLine("I am also here!");
+        //}
     }
 }
