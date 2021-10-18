227e6ff7bdc03531937d89d7048b75ec46424eb6
rivkif770 <frieman@g.jct.ac.il>
2021-10-18 11:54:47 +0300

    a

--- DAL/DataSource.cs
+++ DAL/DataSource.cs
@@ -8,14 +8,12 @@ namespace DalObject
         BaseStation[] ba = new BaseStation[5];
         Package[] pa = new Package[1000];
         Client[] cl = new Client[100];
-        Package[] pa = new Package[1000];
         internal class Config
         {
             internal static int q = 0;
             internal static int b = 0;
             internal static int p = 0;
             internal static int c = 0;
-            internal static int p = 0;
         }
         public static void Initialize()
         {
