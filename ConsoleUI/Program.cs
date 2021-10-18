33e4af80d78d8f2c22a70d5caee9f33eaff26e9f
rivkif770 <frieman@g.jct.ac.il>
2021-10-18 22:37:56 +0300

    The beginning of the main

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,10 +1,131 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IDAL.DO;
 namespace ConsoleUI
 {
-    enum Options {add, update, display, ViewTheLists, exit }
     class Program
     {
+        enum Options { Exit, Add, Update, Display, ViewTheLists }
+        enum InseitOption { Exit, AddBaseStation, AddSkimmer, AddClient,AddPackage }
+        enum UpdateOption { Exit, Affiliation, Collection, Supply, SendLoading, ReleaseCharging }
+        enum OptionsListView { Exit, ViewBaseStation, ViewSkimmer, ViewClient, ViewPackage, ViewUnassignedPackages, ViewFreeBaseStation }
+        enum DisplayOptions { Exit, DisplayBaseStation, DisplaySkimmer, DisplayClient, DisplayPackage }
+        private static void Menu()
+        {
+            Options options;
+            InseitOption inseitOption;
+            UpdateOption updateOption;
+            OptionsListView optionsListView;
+            DisplayOptions displayOptions;
+            do
+            {
+                Console.WriteLine("welcome!" + "option:\n 0-Exit\n 1-Add\n 2-Update\n 3-List View\n 4-Display\n");
+                options = (Options)int.Parse(Console.ReadLine());
+                int id, num, status, CustomerSending, CustomerReceiving, SkimmerOperation;
+                string name, Phone;
+                double longitude, Latitude;
+                DronStatuses mode;
+                WeightCategories Weight;
+                Priorities priority;
+                DateTime TimeDelivery, TimeGlider, collectionTime, TimeRecipient;
+                switch (options)
+                {
+                    case Options.Add:
+                        Console.WriteLine("adding option:\n 0-Exit\n; 1- Add a base station to the list of stations\n" +
+                            " 2- Add a skimmer to the list of existing skimmers\n" +
+                            "3- Admission of a new customer to the customer list\n " +
+                            " 4-Receipt of package for shipment\n");
+                        inseitOption = (InseitOption)int.Parse(Console.ReadLine());
+                        switch (inseitOption)
+                        {
+                            case InseitOption.Exit:
+                                break;
+                            case InseitOption.AddBaseStation:
+                                Console.WriteLine("Enter unique ID number:");
+                                id = int.Parse(Console.ReadLine());
+                                Console.WriteLine("Enter The station name:");
+                                name = (Console.ReadLine());
+                                Console.WriteLine("Enter Several positions of charging:");
+                                num = int.Parse(Console.ReadLine());
+                                Console.WriteLine("Enter longitude:");
+                                longitude = double.Parse(Console.ReadLine());
+                                Console.WriteLine("Enter Latitude:");
+                                Latitude = double.Parse(Console.ReadLine());
+                                //DalObject.DataSource.BaseStationArry[ DataSource.B](id, name, num, longitude, Latitude);
+                                break;
+                            case InseitOption.AddSkimmer:
+                                Console.WriteLine("Enter unique ID number:");
+                                id = int.Parse(Console.ReadLine());
+                                Console.WriteLine("Enter Skimmer model:");
+                                name = (Console.ReadLine());
+                                Console.WriteLine("Enter Weight category 1-low,2-middle,3-heavy:");
+                                Weight = (WeightCategories)int.Parse(Console.ReadLine());
+                                Console.WriteLine("Enter Battery status:");
+                                status = int.Parse(Console.ReadLine());
+                                Console.WriteLine("Enter Skimmer mode:");
+                                mode = (DronStatuses)int.Parse(Console.ReadLine());
+                                //DalObject.DataSource.BaseStationArry[ DataSource.B](id, name, num, longitude, Latitude);
+                                break;
+                            case InseitOption.AddClient:
+                                Console.WriteLine("Enter unique ID number:");
+                                id = int.Parse(Console.ReadLine());
+                                Console.WriteLine("Enter the customer's name:");
+                                name = (Console.ReadLine());
+                                Console.WriteLine("Enter Phone Number:");
+                                Phone = (Console.ReadLine());
+                                Console.WriteLine("Enter longitude:");
+                                longitude = double.Parse(Console.ReadLine());
+                                Console.WriteLine("Enter Latitude:");
+                                Latitude = double.Parse(Console.ReadLine());
+                                //DalObject.DataSource.BaseStationArry[ DataSource.B](id, name, num, longitude, Latitude);
+                                break;
+                            case InseitOption.AddPackage:
+                                Console.WriteLine("Enter unique ID number:");
+                                id = int.Parse(Console.ReadLine());
+                                Console.WriteLine("Enter Sending customer ID:");
+                                CustomerSending = int.Parse(Console.ReadLine());
+                                Console.WriteLine("Receiving customer ID:");
+                                CustomerReceiving = int.Parse(Console.ReadLine());
+                                Console.WriteLine("Enter Weight category 1-low,2-middle,3-heavy:");
+                                Weight = (WeightCategories)int.Parse(Console.ReadLine());
+                                Console.WriteLine("Enter priority:");
+                                priority = (Priorities)int.Parse(Console.ReadLine());
+                                Console.WriteLine("Enter Operator skimmer ID:");
+                                SkimmerOperation = int.Parse(Console.ReadLine());
+                                Console.WriteLine("Enter Time to create a package for delivery:");
+                                TimeDelivery = DateTime.Parse(Console.ReadLine());
+                                Console.WriteLine("Enter Time to assign the package to the glider:");
+                                TimeGlider = DateTime.Parse(Console.ReadLine());
+                                Console.WriteLine("Enter Package collection time from the sender:");
+                                collectionTime = DateTime.Parse(Console.ReadLine());
+                                Console.WriteLine("Enter Time of arrival of the package to the recipient:");
+                                TimeRecipient = DateTime.Parse(Console.ReadLine());
+                                //DalObject.DataSource.BaseStationArry[ DataSource.B](id, name, num, longitude, Latitude);
+                                break;
+                        }
+                        break;
+
+                    case Options.Update:
+                        Console.WriteLine("adding option:\n 0-Exit\n; 1- Assign a package to a skimmer\n" +
+                            " 2- Package collection by skimmer\n" +
+                            "3- ADelivery package to customer\n " +
+                            " 4-Sending a skimmer for charging at a base station\n" +
+                            "5-Release skimmer from charging at base station");
+                        updateOption = (UpdateOption)int.Parse(Console.ReadLine());
+                        switch (updateOption)
+                        {
+                            case UpdateOption.Exit:
+                                break;
+                        }
+                        break;
+                }
+            }
+            while (true);
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine( "Choose one of the following:\n" +
--- DAL/DataSource.cs
+++ DAL/DataSource.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using IDAL.DO;
 namespace DalObject
 {
@@ -22,107 +26,56 @@ namespace DalObject
             for (int i = Config.B; i < 2; i++)
             {
                 DJ.BaseStationArry[i].UniqueID = r.Next(999,10000);
-                
                 DJ.BaseStationArry[i].SeveralPositionsArgument = r.Next(5);
                 DJ.BaseStationArry[i].Latitude = r.Next(-100,100);
                 DJ.BaseStationArry[i].Longitude = r.Next(-100,100);
             }
-            DJ.BaseStationArry[0].StationName = "Jojo1";
-            DJ.BaseStationArry[1].StationName = "Jojo2";
+            DJ.BaseStationArry[0].StationName = "Tamar";
+            DJ.BaseStationArry[1].StationName = "Rimon";
             for (int i = Config.Q ; i < 5; i++)
             {
                 DJ.QuadocopterArry[i].IDNumber = r.Next(99,1000);
                 DJ.QuadocopterArry[i].Battery = r.Next(101);
+                DJ.QuadocopterArry[i].Weight = (WeightCategories)r.Next(3);
+                DJ.QuadocopterArry[i].SkimmerMode = (DronStatuses)r.Next(3);
             }
-           
+
             DJ.QuadocopterArry[0].SkimmerModel = "A20";
-            DJ.QuadocopterArry[0].Weight = WeightCategories.low;
-            DJ.QuadocopterArry[0].SkimmerMode = DronStatuses.free;
-
             DJ.QuadocopterArry[1].SkimmerModel = "A21";
-            DJ.QuadocopterArry[1].Weight = WeightCategories.heavy;
-            DJ.QuadocopterArry[1].SkimmerMode = DronStatuses.shipping;
-
             DJ.QuadocopterArry[2].SkimmerModel = "A22";
-            DJ.QuadocopterArry[2].Weight = WeightCategories.middle;
-            DJ.QuadocopterArry[2].SkimmerMode = DronStatuses.shipping;
-
             DJ.QuadocopterArry[3].SkimmerModel = "A23";
-            DJ.QuadocopterArry[3].Weight = WeightCategories.middle;
-            DJ.QuadocopterArry[3].SkimmerMode = DronStatuses.maintenance;
-
-
             DJ.QuadocopterArry[4].SkimmerModel = "A24";
-            DJ.QuadocopterArry[4].Weight = WeightCategories.low;
-            DJ.QuadocopterArry[4].SkimmerMode = DronStatuses.maintenance;
 
             for (int i = Config.C; i < 10; i++)
             {
                 DJ.ClientArry[i].Latitude = r.Next(-100, 100);
                 DJ.ClientArry[i].Longitude = r.Next(-100, 100);
                 DJ.ClientArry[i].ID = r.Next(999999999, 1000000000);
+                DJ.ClientArry[i].Telephone = $"0{r.Next(50,59)}-{r.Next(1000000,10000000)}";
             }
             DJ.ClientArry[0].Name = "David";
-            DJ.ClientArry[0].Telephone = "052-4578107";
             DJ.ClientArry[1].Name = "Sara";
-            DJ.ClientArry[0].Telephone = "053-5223655";
             DJ.ClientArry[2].Name = "Chaim";
-            DJ.ClientArry[0].Telephone = "052-3222781";
             DJ.ClientArry[3].Name = "Moshe";
-            DJ.ClientArry[0].Telephone = "054-67703211";
             DJ.ClientArry[4].Name = "Jon";
-            DJ.ClientArry[0].Telephone = "052-8826402";
             DJ.ClientArry[5].Name = "Rivki";
-            DJ.ClientArry[0].Telephone = "055-0399517";
             DJ.ClientArry[6].Name = "Efrat";
-            DJ.ClientArry[0].Telephone = "058-1770441";
             DJ.ClientArry[7].Name = "Chani";
-            DJ.ClientArry[0].Telephone = "053-5722946";
             DJ.ClientArry[8].Name = "Goldi";
-            DJ.ClientArry[0].Telephone = "052-2044763";
             DJ.ClientArry[9].Name = "Pazit";
-            DJ.ClientArry[0].Telephone = "055-2584600";
 
             for (int i = Config.P; i < 10; i++)
             {
                 DJ.PackageArry[i].ID = r.Next(99999, 100000);
                 DJ.PackageArry[i].IDSender =DJ.ClientArry[r.Next(6)].ID ;
                 DJ.PackageArry[i].IDgets = DJ.ClientArry[r.Next(6)].ID;
+                DJ.PackageArry[i].Weight = (WeightCategories)r.Next(3);
+                DJ.PackageArry[i].priority = (Priorities)r.Next(3);
                 DJ.PackageArry[i].PackageCreationTime = new DateTime(2021, r.Next(1, 13), r.Next(1, 30), r.Next(1, 24), r.Next(1, 61),r.Next(1, 61));
                 DJ.PackageArry[i].TimeAssignGlider = new DateTime(2021, r.Next(1, 13), r.Next(1, 30), r.Next(1, 24), r.Next(1, 61), r.Next(1, 61));
                 DJ.PackageArry[i].PackageCollectionTime = new DateTime(2021, r.Next(1, 13), r.Next(1, 30), r.Next(1, 24), r.Next(1, 61), r.Next(1, 61));
                 DJ.PackageArry[i].TimeArrivalRecipient = new DateTime(2021, r.Next(1, 13), r.Next(1, 30), r.Next(1, 24), r.Next(1, 61), r.Next(1, 61));
             }
-
-            DJ.PackageArry[0].Weight = WeightCategories.heavy;
-            DJ.PackageArry[0].priority = Priorities.emergency;
-
-            DJ.PackageArry[1].Weight = WeightCategories.middle;
-            DJ.PackageArry[1].priority = Priorities.emergency;
-
-            DJ.PackageArry[2].Weight = WeightCategories.low;
-            DJ.PackageArry[2].priority = Priorities.regular;
-
-            DJ.PackageArry[3].Weight = WeightCategories.low;
-            DJ.PackageArry[3].priority = Priorities.regular;
-
-            DJ.PackageArry[4].Weight = WeightCategories.heavy;
-            DJ.PackageArry[4].priority = Priorities.emergency;
-
-            DJ.PackageArry[5].Weight = WeightCategories.middle;
-            DJ.PackageArry[5].priority = Priorities.fast;
-
-            DJ.PackageArry[6].Weight = WeightCategories.low;
-            DJ.PackageArry[6].priority = Priorities.regular;
-
-            DJ.PackageArry[7].Weight = WeightCategories.low;
-            DJ.PackageArry[7].priority = Priorities.emergency;
-
-            DJ.PackageArry[8].Weight = WeightCategories.heavy;
-            DJ.PackageArry[8].priority = Priorities.regular;
-
-            DJ.PackageArry[9].Weight = WeightCategories.heavy;
-            DJ.PackageArry[9].priority = Priorities.fast;
         }
     }
 }
