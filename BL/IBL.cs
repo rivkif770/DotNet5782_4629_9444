b046665d375061562fb36780b40474a37e92ca6a
rivki <frieman@g.jct.ac.il>
2021-11-23 22:38:04 +0200

    321

--- BL/IBL.cs
+++ BL/IBL.cs
@@ -1,30 +1,35 @@
 ﻿using IBL.BO;
+using System.Collections.Generic;
 
 namespace IBL
 {
     public interface IBL
     {
-        void AddCustomer(Customer newCustomer);
-        Customer GetCustomer(int id);
-        public void UpdateCustomerData(int id, string name, string phone);
-
-        public void ReleaseSkimmerFromCharging(int id, double ChargingTime);
-        void AddSkimmer(Skimmer newSkimmer, int station);
-        public SkimmerToList GetSkimmer(int id);
-        public void UpdateSkimmerName(int ids, string name);
-        public void SendingSkimmerForCharging(int id);
-
         void AddBaseStation(BaseStation newBaseStation);
         public void UpdateBaseStation(int id, string name, string countOfChargingStations);
         BaseStation GetBeseStation(int id);
+        IEnumerable<BaseStationToList> GetBaseStationList();
+        IEnumerable<BaseStationToList> GetBaseStationFreeCharging();
+
         void AddCustomer(Customer newCustomer);
-        void AddPackage(Package newPackage);
-        void AddSkimmer(Skimmer newSkimmer, int station);
         Customer GetCustomer(int id);
-        Customer GetPackage(int id);
-        Skimmer GetSkimmer(int id);
+        public void UpdateCustomerData(int id, string name, string phone);
+        IEnumerable<CustomerToList> GetCustomerList();
+
+        void AddPackage(Package newPackage);
         public void CollectingPackageBySkimmer(int id);
         public void AssigningPackageToSkimmer(int id);
         public void DeliveryOfPackageBySkimmer(int id);
+        IEnumerable<PackageToList> GetPackageList();
+        IEnumerable<PackageToList> GetPackagesWithoutSkimmer();
+
+        void AddSkimmer(Skimmer newSkimmer, int station);
+        public SkimmerToList GetSkimmer(int id);
+        public void ReleaseSkimmerFromCharging(int id, double ChargingTime);
+        public void UpdateSkimmerName(int ids, string name);
+        public void SendingSkimmerForCharging(int id);
+        IEnumerable<SkimmerToList> GetSkimmerList();
+
+  
     }
 }
